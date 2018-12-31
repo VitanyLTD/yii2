79c5e243a8ad293c8926d3d6c7be9d0ad75c1f44
Matthijs Wiersma <mwiersma@elitebomatthijs.home>
2018-12-31 15:58:55 +0100

    Update databaseconfiguration

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=yii2',
+    'username' => 'yii2user',
+    'password' => 'SD2kLk!2Z339?&r@',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
