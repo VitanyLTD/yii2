18c32c1dc800f503bc10f5441073cc504b99318f
Matthijs Wiersma <mwiersma@elitebomatthijs.home>
2018-12-31 16:04:19 +0100

    Update database-host settings

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2',
+    'dsn' => 'mysql:host=127.0.0.1;port=8889;dbname=yii2',
     'username' => 'yii2user',
     'password' => 'SD2kLk!2Z339?&r@',
     'charset' => 'utf8',
